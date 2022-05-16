be26b5b61977086d20999c4e4a0ec18ca1a5e5d1
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-17 01:19:06 +0530

    Connected to Firebase and signup setup code is done.

--- app/src/main/java/com/rishabhkumar/whatsapp_clone/Fragment/SignUp.kt
+++ app/src/main/java/com/rishabhkumar/whatsapp_clone/Fragment/SignUp.kt
@@ -1,60 +1,86 @@
 package com.rishabhkumar.whatsapp_clone.Fragment
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import com.rishabhkumar.whatsapp_clone.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUp.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUp : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var enterEmail: TextInputEditText
+    private lateinit var enterPassword: TextInputEditText
+    private lateinit var confirmPassword: TextInputEditText
+    private lateinit var signUpButton: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var fauth : FirebaseAuth
+    private lateinit var fstore : FirebaseFirestore
+    private lateinit var db : DocumentReference
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        enterEmail = view.findViewById(R.id.edtSignUPEmail)
+        enterPassword = view.findViewById(R.id.edtSignUPPassword)
+        confirmPassword = view.findViewById(R.id.edtSignUPConfirmPassword)
+        signUpButton = view.findViewById(R.id.btnSignUP)
+        progressBar = view.findViewById(R.id.signUPProgressBar)
+
+        fauth = FirebaseAuth.getInstance()
+        fstore = FirebaseFirestore.getInstance()
+        signUpButton.setOnClickListener{
+            val email = enterEmail.text.toString()
+            val password = enterPassword.text.toString()
+            val confirmPasswordd = confirmPassword.text.toString()
+            if(TextUtils.isEmpty(email)){
+                enterEmail.error = "Email is required."
+            }else if(TextUtils.isEmpty(password)){
+                enterPassword.error = "Password is required."
+            }else if(TextUtils.isEmpty(confirmPasswordd)){
+                confirmPassword.error = "Please re-enter password"
+            }else if(TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPasswordd)){
+                enterPassword.error = "Password is required."
+                confirmPassword.error = "Please re-enter password"
+            }else if(password.length < 8){
+                enterPassword.error = "Password must be greater than 8 letters"
+            }else if(password != confirmPasswordd){
+                confirmPassword.error = "Password must be same"
+            }else{
+                progressBar.visibility = View.VISIBLE
+                createAccount(email,password)
+            }
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUp.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUp().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    private fun createAccount(email: String, password: String) {
+        fauth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
+            task->
+            if(task.isSuccessful){
+                val userInfo = fauth.currentUser?.uid
+                db = fstore.collection("users").document(userInfo.toString())
+                val obj = mutableMapOf<String,String>()
+                obj["userEmail"] = email
+                obj["userPassword"] = password
+                db.set(obj).addOnSuccessListener {
+                    Log.d("onSuccess","User Created Successfully")
+                    progressBar.visibility = View.GONE
                 }
             }
+        }
     }
 }
