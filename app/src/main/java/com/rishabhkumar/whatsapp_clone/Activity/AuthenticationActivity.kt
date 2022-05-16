14ee71a3272ea11a97628ff1aea42dc67b8692b0
Rishabh Kumar <crishabhkumar@gmail.com>
2022-05-16 19:38:10 +0530

    Splash screen and authentication screen xml is done.

---
+++ app/src/main/java/com/rishabhkumar/whatsapp_clone/Activity/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.rishabhkumar.whatsapp_clone.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rishabhkumar.whatsapp_clone.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
